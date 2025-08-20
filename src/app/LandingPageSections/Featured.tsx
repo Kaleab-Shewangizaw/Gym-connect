4c1852b17e3b3cfb97124565ea81f317227342c1
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-08-20 21:33:50 +0300

    trainers card edit

--- src/app/LandingPageSections/Featured.tsx
+++ src/app/LandingPageSections/Featured.tsx
@@ -5,7 +5,8 @@ import { Button } from "@/components/ui/button";
 import { Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { BsStarFill } from "react-icons/bs";
+import { BsInstagram, BsStarFill, BsTiktok, BsTwitterX } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 
 const gyms = [
   {
@@ -53,26 +54,45 @@ const gyms = [
 const trainers = [
   {
     name: "Alex Carter",
+    rating: 4.5,
     image: "/trainers/t1.jpg",
     skills: ["Strength Training", "HIIT"],
   },
   {
     name: "Maya Lee",
+    rating: 4.5,
     image: "/trainers/t2.jpg",
     skills: ["Yoga", "Pilates"],
   },
   {
     name: "Jake Smith",
+    rating: 4.5,
     image: "/trainers/t3.jpg",
+    skills: ["Cardio", "CrossFit"],
+  },
+  {
+    name: "Kebedech Chane",
+    rating: 4.5,
+    image: "/trainers/t4.jpg",
+    skills: ["Cardio", "CrossFit"],
+  },
+  {
+    name: "Demelew",
+    rating: 4.5,
+    image: "/trainers/t5.jpg",
     skills: ["Cardio", "CrossFit"],
   },
 ];
 
 export default function FeaturedSection() {
   return (
-    <section className="py-16 pt-5 w-[95%] md:w-[80%] mx-auto">
+    <section className="py-16  w-[95%] md:w-[80%] mx-auto relative">
+      <div className="absolute bg-fuchsia-600 w-0 shadow-[0px_0px_300px_70px_#c800de] bottom-50 right-50"></div>
+      <div className="absolute bg-fuchsia-600 w-0 shadow-[0px_0px_300px_60px_#c800de] top-240 left-50"></div>
+      <div className="absolute bg-fuchsia-600 w-0 shadow-[0px_0px_300px_80px_#008236] top-180 right-50"></div>
+      <div className="absolute bg-fuchsia-600 w-0 shadow-[0px_0px_300px_70px_#008236] bottom-150 left-50"></div>
       <div className="">
-        <h2 className="text-3xl font-bold text-center text-white mb-10">
+        <h2 className="text-3xl font-bold text-center text-white mb-5">
           Featured Gyms & Trainers
         </h2>
         {/* Gyms Grid */}
@@ -149,13 +169,13 @@ export default function FeaturedSection() {
         {/* Trainers Grid */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Trainers</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-3 gap-y-6">
             {trainers.map((trainer) => (
               <div
                 key={trainer.name}
-                className=" text-white rounded-lg shadow p-4 flex flex-col items-center"
+                className=" text-white rounded-lg shadow  px-2 pb-2 border w-45 md:w-50  lg:w-55  flex flex-col items-center justify-between gap-2"
               >
-                <div className="relative w-60 h-100 rounded-md  overflow-hidden">
+                <div className="relative w-45 md:w-50 md:h-90 h-60 lg:w-55 lg:h-95 rounded-md  overflow-hidden">
                   <Image
                     src={trainer.image}
                     alt={trainer.name}
@@ -163,16 +183,37 @@ export default function FeaturedSection() {
                     className="object-cover "
                   />
                 </div>
-                <h4 className="font-bold text-lg mb-1">{trainer.name}</h4>
+                <div className="flex items-center justify-between w-full flex-wrap">
+                  <h4 className="text-lg font-bold   text-gray-200">
+                    {trainer.name}
+                  </h4>
+                  <div className="flex flex-wrap gap-2">
+                    <BsStarFill className="text-yellow-500 text-lg " />{" "}
+                    <p className="text-gray-300 text-sm flex items-center font-bold">
+                      {" "}
+                      {trainer.rating}
+                    </p>
+                  </div>
+                </div>
                 <div className="flex flex-wrap gap-2 justify-center">
                   {trainer.skills.map((skill) => (
                     <span
                       key={skill}
-                      className="bg-blue-900 text-blue-300 px-2 py-1 rounded text-xs font-medium"
+                      className="bg-blue-900 text-blue-100 px-2 py-1 rounded text-xs font-medium"
                     >
                       {skill}
                     </span>
                   ))}
+                </div>
+                <div className="w-full flex mt-3 justify-around">
+                  <Button>Book</Button>
+                  <Button>View</Button>
+                </div>
+                <div className="flex flex-wrap gap-5 mt-5 text-lg  justify-between  w-full self-end ">
+                  <BsInstagram />
+                  <FaFacebookF />
+                  <BsTwitterX />
+                  <BsTiktok />
                 </div>
               </div>
             ))}
--- src/app/LandingPageSections/Navbar.tsx
+++ src/app/LandingPageSections/Navbar.tsx
@@ -9,7 +9,7 @@ export default function HeroNavbar() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="flex items-end justify-between py-2 px-2 w-[95%] md:w-[80%] z-10  mx-auto  sticky top-0 bg-black/60 rounded-md"
+      className="flex items-end justify-between py-2 px-2 w-[95%] md:w-[80%] z-10  mx-auto  sticky top-0 bg-black/70 rounded-md"
     >
       <div className="text-3xl  relative px-1 py-1.5 overflow-hidden bg-green-700 rounded-tr-md rounded-br-xl rounded-tl-xl ">
         <BiDumbbell className="rotate-z-30 text-white" />
@@ -60,7 +60,7 @@ export default function HeroNavbar() {
           <motion.button
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.3 }}
-            className=" px-2 py-0.5 cursor-pointer  rounded-md font-bold text-sm   border border-green-700 text-green-700"
+            className=" px-2 py-0.5 cursor-pointer  rounded-md font-bold text-sm   border border-green-700 text-white"
           >
             Sign up
           </motion.button>
