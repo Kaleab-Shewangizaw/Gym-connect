78a66816504f3a94e220b22d828b869cb4fb3599
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-08-20 12:22:10 +0300

    added some animation to the how it works section

--- src/app/LandingPageSections/HowItWorks.tsx
+++ src/app/LandingPageSections/HowItWorks.tsx
@@ -1,15 +1,57 @@
+"use client";
+
+import { motion, useAnimation, useInView } from "framer-motion";
+
 import { MapPin, Dumbbell, CalendarCheck } from "lucide-react";
+import { useEffect, useRef } from "react";
 
 export default function HowItWorksSection() {
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const inView = useInView(sectionRef, { once: true, margin: "-150px" });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (inView) controls.start("visible");
+  }, [inView, controls]);
   return (
-    <section className="py-16 relative w-[95%] md:w-[80%] mx-auto">
+    <motion.section className="py-16 relative w-[95%] md:w-[80%] mx-auto">
       <div className="absolute bg-fuchsia-600 w-0 shadow-[0px_0px_300px_70px_#c800de] bottom-25 left-50"></div>
       <div className="absolute bg-fuchsia-600 w-0 shadow-[0px_0px_300px_70px_#008236] top-20 right-50"></div>
-      <div className=" ">
-        <h2 className="text-3xl font-bold text-center mb-10">How It Works</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={{
+          hidden: {},
+          visible: { transition: { staggerChildren: 0.1 } },
+        }}
+        className=" "
+      >
+        <motion.h2
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
+          className="text-3xl font-bold text-center mb-10"
+        >
+          How It Works
+        </motion.h2>
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-3 gap-8"
+          ref={sectionRef}
+          initial="hidden"
+          animate={controls}
+          variants={{
+            hidden: {},
+            visible: { transition: { staggerChildren: 0.3 } },
+          }}
+        >
           {/* Step 1 */}
-          <div className="flex flex-col items-center text-center">
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className="flex flex-col items-center text-center"
+          >
             <div className="bg-blue-900 p-4 rounded-full mb-4">
               <MapPin className="w-8 h-8  text-blue-300" />
             </div>
@@ -17,9 +59,15 @@ export default function HowItWorksSection() {
             <p className=" text-gray-400">
               Find gyms by location, capacity, and price.
             </p>
-          </div>
+          </motion.div>
           {/* Step 2 */}
-          <div className="flex flex-col items-center text-center">
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className="flex flex-col items-center text-center"
+          >
             <div className="bg-green-900 p-4 rounded-full mb-4">
               <Dumbbell className="w-8 h-8 text-green-300" />
             </div>
@@ -29,9 +77,15 @@ export default function HowItWorksSection() {
             <p className=" text-gray-400">
               Select a trainer or just book a gym slot.
             </p>
-          </div>
+          </motion.div>
           {/* Step 3 */}
-          <div className="flex flex-col items-center text-center">
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className="flex flex-col items-center text-center"
+          >
             <div className="bg-purple-900 p-4 rounded-full mb-4">
               <CalendarCheck className="w-8 h-8 text-purple-300" />
             </div>
@@ -39,9 +93,9 @@ export default function HowItWorksSection() {
             <p className="text-gray-400">
               Confirm your spot instantly and start training.
             </p>
-          </div>
-        </div>
-      </div>
-    </section>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </motion.section>
   );
 }
