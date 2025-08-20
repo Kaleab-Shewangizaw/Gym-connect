4410ad080fc301d429f7360667844eb58b41688a
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-08-20 12:29:48 +0300

    how it works section once true added

--- src/app/LandingPageSections/HowItWorks.tsx
+++ src/app/LandingPageSections/HowItWorks.tsx
@@ -29,6 +29,7 @@ export default function HowItWorksSection() {
         <motion.h2
           initial={{ opacity: 0, y: -50 }}
           whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
           transition={{ duration: 1 }}
           className="text-3xl font-bold text-center mb-10"
         >
